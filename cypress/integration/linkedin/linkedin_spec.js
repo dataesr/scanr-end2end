21393ec22591e26a5face4d9bf7e47764724f2ba
paulinegc <francasix@outlook.com>
2021-03-26 22:11:50 +0100

    linkedin profile tests

---
+++ cypress/integration/linkedin/linkedin_spec.js
@@ -0,0 +1,14 @@
+describe('Profile #1', () => {
+    it('Looks for opportunities', () => {
+        cy.visit('www.linkedin.com');
+        cy.get('.nav__cta-container .nav__button-secondary').click();
+        cy.get('#username').type('');
+        cy.get('#password').type('');
+        cy.get('.login__form_action_container .btn__primary--large.from__button--floating').click();
+
+        cy.visit(Cypress.env('searchUrl'));
+        cy.get('.reusable-search__entity-results-list > li').eq(0).click();
+        cy.wait(3000);
+        cy.get('.poc-opportunities-card__text-content').should('not.exist');
+    });
+});
