fed50a041989f30d6f5e6e12d7536ce55e6a509f
paulinegc <francasix@outlook.com>
2021-03-29 15:58:48 +0200

    add test conrtibute

--- cypress/integration/modal-suggest_spec.js
+++ cypress/integration/modal-suggest_spec.js
@@ -6,10 +6,22 @@ describe('Modal Suggestion Test', () => {
         cy.get('#Production .fa-plus-square').click();
         cy.get('.ReactModal__Content').should('exist');
     });
-    // it('Selects a publication', () => {
-    //     cy.get('#Production .fa-plus-square').click();
-    //     const $element = cy.get('input[type="checkbox"]').first();
-    //     $element.click();
-    //     $element.should('be.checked');
-    // });
+    it('Selects a publication', () => {
+        cy.get('#Production .fa-plus-square').click();
+        cy.get('.ReactModal__Content .fa-square').first().click();
+
+        cy.get('.fa-check-square').should(($checkboxChecked) => {
+            expect($checkboxChecked).to.have.length(1);
+        });
+    });
+    it('Contributes a publication', () => {
+        cy.get('#Production .fa-plus-square').click();
+        cy.get('.ReactModal__Content .fa-square').eq(2).click();
+        cy.contains('Valider').click();
+        cy.get('#email').type('test@endtoend.com');
+        cy.get('#message').type('Message test!');
+        cy.contains('Envoyer').click();
+
+        cy.get('.alert-dark span').should('eq', 'Merci pour vos contributions')
+    });
 });
