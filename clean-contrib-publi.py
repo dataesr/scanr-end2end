78fe291c2b4fd17dbd91446e9139388c5ade44f4
paulinegc <francasix@outlook.com>
2021-03-29 14:35:02 +0200

    action test init

---
+++ clean-contrib-publi.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+import numpy as np
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASEDIR, '.env'))
+
+HEADERS = {'Authorization':os.getenv('HEADERS_AUTH_BASIC')}
+SCANR_API_URL = os.getenv('SCANR_API_URL_CONTRIBUTE')
+
+url = str(SCANR_API_URL) + '?max_results=500&where={"email": "test@endtoend.com"}'
+r = requests.get(url, headers=HEADERS)
+data = r.json().get('data')
+total = r.json().get('meta', {}).get('total')
+
+print(f'Total number of documents retured: {total}')
+
+for doc in data:
+    _id = doc.get('_id')
+    url = f'{SCANR_API_URL}/{_id}'
+    try:
+        r = requests.delete(url, headers=HEADERS)
+    except:
+        print(r.text)
--- cypress/integration/modal-suggest_spec.js
+++ cypress/integration/modal-suggest_spec.js
@@ -1,15 +1,15 @@
 describe('Modal Suggestion Test', () => {
     beforeEach(() => {
-        cy.visit('/person/idref030959829');
+        cy.visit(`/person/${Cypress.env('personId')}`);
     });
     it('Opens modal', () => {
-        cy.get('.cy-suggest').click();
-        cy.get('.ReactModal__Content').should('exist')
+        cy.get('#Production .fa-plus-square').click();
+        cy.get('.ReactModal__Content').should('exist');
     });
-    it('Selects a publication', () => {
-        cy.get('.cy-suggest').click();
-        const $element = cy.get('div[class*=ProductionList__Item__]').first()
-        $element.click()
-        $element.find('input').should('be.checked')
-    });
+    // it('Selects a publication', () => {
+    //     cy.get('#Production .fa-plus-square').click();
+    //     const $element = cy.get('input[type="checkbox"]').first();
+    //     $element.click();
+    //     $element.should('be.checked');
+    // });
 });
